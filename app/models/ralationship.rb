3f892f0f44ee4ae52c32efd1437b20bec70e2f39
rw <mjbjb032@gmail.com>
2018-03-30 07:20:24 +0000

    follow, unfollow

---
+++ app/models/ralationship.rb
@@ -0,0 +1,8 @@
+class Ralationship < ApplicationRecord
+  belongs_to :user
+  belongs_to :follow, class_name: 'user'
+  
+  validates :user_id, presence: true
+  validates :follow_id, presence: true
+
+end
